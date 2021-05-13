36ca757b3e2b712e1ec05939e12dda94855d8dcb
hustine316 <austinefrank14@gmail.com>
2021-05-13 22:36:41 +0200

    homepage image update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
 import classes from "./Home.css";
-import Luggage from "./../../assets/img/landing-image.png";
+import Shirt from "./../../assets/img/landing-image.png";
 import { NavLink } from "react-router-dom";
 
 export default class Home extends Component {
   render() {
     return (
       <div className={classes.Container}>
-        <img src={Luggage} alt="home" className={classes.Img} />
+        <img src={Shirt} alt="home" className={classes.Img} />
         <div className={classes.Overlay} />
         <div className={classes.Headding}>
-          <h1 className={classes.Title}>
-            Thoughtful standards for modern travel
-          </h1>
+          <h1 className={classes.Title}>Choose the perfect outfit</h1>
           <NavLink to="/shop">
             <button className={classes.Btn}>shop now</button>
           </NavLink>
