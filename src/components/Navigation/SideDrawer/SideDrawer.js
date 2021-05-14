992cf863b97c7e8c158f4c9f1e2c771505f32c13
hustine316 <austinefrank14@gmail.com>
2021-05-14 09:55:50 +0200

    home

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,31 +1,36 @@
-import React from 'react';
+import React from "react";
 import { NavLink } from "react-router-dom";
 import Navigationitem from "./../Navigationitems/Navigationitem/Navigationitem";
 import Logo from "./../../Logo/Logo";
 import classes from "./SideDrawer.css";
 const SideDrawer = (props) => {
   let drawerClasses = [classes.SideDrawer];
-  if(props.show){
-    drawerClasses = [classes.SideDrawer,classes.Open]
+  if (props.show) {
+    drawerClasses = [classes.SideDrawer, classes.Open];
   }
   return (
-    <nav className= {drawerClasses.join(" ")}>
-     <div className={classes.Logo}>
+    <nav className={drawerClasses.join(" ")}>
+      {/* <div className={classes.Logo}>
         <NavLink to="/">
           <Logo click={props.click}/>
         </NavLink>
-      </div>
+      </div> */}
       <ul className={classes.NavigationItems}>
-      <Navigationitem link="/shop" click={props.click} >
-        Shop
-      </Navigationitem>
-      <Navigationitem link="/about" click={props.click}>About</Navigationitem>
-      <Navigationitem link="/cart" click={props.click}>
-       Cart
-      </Navigationitem>
-    </ul>
+        <Navigationitem link="/" click={props.click}>
+          Home
+        </Navigationitem>
+        <Navigationitem link="/shop" click={props.click}>
+          Shop
+        </Navigationitem>
+        <Navigationitem link="/about" click={props.click}>
+          About
+        </Navigationitem>
+        <Navigationitem link="/cart" click={props.click}>
+          Cart
+        </Navigationitem>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default SideDrawer
+export default SideDrawer;
