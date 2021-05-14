5fbc67566d0024d3dbf714de07ffd796bf3b5cd6
hustine316 <austinefrank14@gmail.com>
2021-05-14 13:54:48 +0200

    about us update

--- src/components/Cart/EmptyCart/EmptyCart.js
+++ src/components/Cart/EmptyCart/EmptyCart.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 import Title from "./../../Title/Title";
 const EmptyCart = () => {
   return (
     <div>
-      <Title name="Your Cart is Empty"/>
+      <Title name="Cart is Empty" />
     </div>
-  )
-}
+  );
+};
 
-export default EmptyCart
+export default EmptyCart;
